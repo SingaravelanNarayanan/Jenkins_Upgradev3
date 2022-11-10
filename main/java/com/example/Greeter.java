8d4457644eb402a57da15628d925c347d8ed796d
Ubuntu <ubuntu@ip-172-31-25-45.ec2.internal>
2022-11-10 14:15:06 +0000

    new commit

--- main/java/com/example/Greeter.java
+++ main/java/com/example/Greeter.java
@@ -21,7 +21,7 @@ public class Greeter {
   
   public static void main(String args[]){
     
-	  System.out.println("Hello from Main Method & Good Morning from singaravelan to all!!!");
+	  System.out.println("Hello from Main Method & Good Morning everyone");
   }
 	
 }
